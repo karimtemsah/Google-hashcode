4895c841429bec5990e072d5409e320f085a92ff
Karim Temsah <>
2018-03-01 21:38:27 +0100

    min distance strategy

--- simulation/strategy/min_distance.py
+++ simulation/strategy/min_distance.py
@@ -1,4 +1,19 @@
 class MinDistance:
-    def run(self, city):
 
+    def run(self, city):
+        city.rides = sorted(city.rides, key=lambda ride: ride.earlieststart)
+        for ride in city.rides:
+
+            min_distance = city.rows * 2
+            car_id = city.cars
+            for car in city.cars:
+                distance_car_to_ride = abs(car.position[0] - ride.start[0]) + abs(car.position[1] - ride.start[1])
+                if distance_car_to_ride < min_distance and car.simulation_step + distance_car_to_ride + ride.duration <= ride.latestend:
+                    min_distance = distance_car_to_ride
+                    car_id = car.id
+            if car_id == city.cars:
+                continue
+            city.cars[car_id].rides.append(ride)
+            city.cars[car_id].simulation_step = city.cars[car_id].simulation_step + ride.duration + min_distance
+            city.cars[car_id].position = ride.end
         return city

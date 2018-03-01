951a3e5675527fbbda0b19ce81c9f2a95030e410
Daniel Schosser <daniel@crashtest-security.com>
2018-03-01 21:36:47 +0100

    optimize percentage of prefered rides

--- simulation/strategy/earliest_start_no_long_rides.py
+++ simulation/strategy/earliest_start_no_long_rides.py
@@ -5,7 +5,15 @@ class EarliestStartNoLongRidesStrategy:
         # Remove all totally long rides
         city.rides = sorted(city.rides, key=lambda ride: ride.duration)
 
-        ninetieth_percentile = int(0.95 * len(city.rides))
+        # 0.78 - 5.303.569 points
+        # 0.76 - 5.345.358 points
+        # 0.75 - 5.371.796 points
+        # 0.74 - 5.382.173 points
+        # 0.73 - 5.318.844 points
+        # 0.7  - 5.193.304 points
+        # 0.6  - 4.299.974 points
+
+        ninetieth_percentile = int(0.74 * len(city.rides))
         rides_short = city.rides[:ninetieth_percentile]
         rides_long = city.rides[ninetieth_percentile:]
         

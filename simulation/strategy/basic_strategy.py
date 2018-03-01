bb4b26d3a394e384c3d8d2487c516df500dc48ab
Manuel <jah-bru@bruzzzla.de>
2018-03-01 19:42:53 +0100

    basic strategy

--- simulation/strategy/basic_strategy.py
+++ simulation/strategy/basic_strategy.py
@@ -1,5 +1,12 @@
+from ..models import Car, City, Ride
+
+
 class BasicStrategy:
 
-    def run(self):
-        # TODO: Implement
-        pass
+    def run(self, city):
+
+        availible_rides = city.rides
+        city.cars = availible_rides
+        city.cars[0].rides.append(availible_rides)
+
+        return city

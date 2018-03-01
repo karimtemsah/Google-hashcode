776baa71a0f4f212e743daffb4cd11775c3390c3
Manuel <jah-bru@bruzzzla.de>
2018-03-01 20:56:35 +0100

    new strategy

--- simulation/strategy/random_car_strategy.py
+++ simulation/strategy/random_car_strategy.py
@@ -6,7 +6,7 @@ class RandomCarStrategy:
     def run(self, city):
 
         for ride in city.rides:
-            car_number = randint(0, city.fleet_size-1)
+            car_number = randint(0, city.fleet_size+1)
             city.cars[car_number].rides.append(ride)
 
         return city

ebeb109eb6aecddeb8dae6621d60e279acb9ed46
Janosch Maier <janosch@crashtest-security.com>
2018-03-01 21:40:59 +0100

    creat get_nearest_best_car method

--- simulation/models/models.py
+++ simulation/models/models.py
@@ -7,6 +7,20 @@ class Ride:
         self.end = [endrow, endcolumn]
         self.duration = abs(startrow - endrow) + abs(startcolumn - endcolumn)
 
+    def get_nearest_best_car(self, cars):
+        best_car = None
+
+        for car in cars:
+            if best_car == None:
+                best_car = car
+                continue
+
+            if car.get_ride_end_time(self) < best_car.get_ride_end_time(self) and \
+                    car.get_ride_end_time(self) <= self.latestend:
+                best_car = car
+
+        return best_car    
+
 
 class Car:
     def __init__(self, id):

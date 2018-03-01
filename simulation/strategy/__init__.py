6b4fc9124d10ba1d9706374cac4e0bc5c4ab1ab4
Janosch Maier <janosch@crashtest-security.com>
2018-03-01 21:12:17 +0100

    add fill car strategy

--- simulation/strategy/__init__.py
+++ simulation/strategy/__init__.py
@@ -1,5 +1,6 @@
 from .basic_strategy import BasicStrategy
 from .earliest_start_strategy import EarliestStartStrategy
 from .closest_ride_next import ClosestRideNextStrategy
+from .fill_car_strategy import FillCarStrategy
 
-__all__ = ('BasicStrategy', 'EarliestStartStrategy', 'ClosestRideNextStrategy')
+__all__ = ('BasicStrategy', 'EarliestStartStrategy', 'ClosestRideNextStrategy', 'FillCarStrategy')
--- simulation/strategy/basic_strategy.py
+++ simulation/strategy/basic_strategy.py
@@ -2,7 +2,6 @@ class BasicStrategy:
 
     def run(self, city):
 
-
         for ride in city.rides:
             city.cars[0].rides.append(ride)
 

f0d0b112db3ad377f0723cfcf37c7609e981afb7
Daniel Schosser <daniel@crashtest-security.com>
2018-03-01 19:48:20 +0100

    add closest ride strategy

---
+++ simulation/strategy/closest_ride_next.py
@@ -0,0 +1,17 @@
+class ClosestRideNext:
+
+    def run(self, city):
+        city.rides = sorted(city.rides, key=lambda ride: ride.start)
+        next_car = 0
+
+        for ride in city.rides:
+
+            distance_car_to_ride = abs(city.cars[next_car].position[0] - ride.start[0]) + abs(city.cars[next_car].position[1] - ride.start[1])
+
+            if city.cars[next_car].simulation_step <= ride.latestend-ride.duration and ride.earlieststart > city.cars[next_car].simulation_step + distance_car_to_ride:
+                city.cars[next_car].rides.append(ride)
+                city.cars[next_car].simulation_step = city.cars[next_car].simulation_step + ride.duration
+                city.cars[next_car].position = ride.end
+                next_car = (next_car + 1) % city.fleet_size
+
+        return city

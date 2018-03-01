1bd3eeb4f3b42e3190622a34adf61c50346db444
Daniel Schosser <daniel@crashtest-security.com>
2018-03-01 21:14:59 +0100

    Add new strategies

--- simulation/strategy/__init__.py
+++ simulation/strategy/__init__.py
@@ -2,5 +2,8 @@ from .basic_strategy import BasicStrategy
 from .earliest_start_strategy import EarliestStartStrategy
 from .closest_ride_next import ClosestRideNextStrategy
 from .fill_car_strategy import FillCarStrategy
+from .shortest_distance import ShortestDistance
+from .improved_strategy import ImprovedStrategy
+from .random_car_strategy import RandomCarStrategy
 
-__all__ = ('BasicStrategy', 'EarliestStartStrategy', 'ClosestRideNextStrategy', 'FillCarStrategy')
+__all__ = ('BasicStrategy', 'EarliestStartStrategy', 'ClosestRideNextStrategy', 'FillCarStrategy', 'ShortestDistance', 'ImprovedStrategy', 'RandomCarStrategy')
